83967d1b0d11934ed08332d64660d60e97fb84af
Q <>
2021-05-05 20:04:24 +0800

    update init script

--- pinpoint-2.1.0/pinpoint-hbase/build/scripts/initialize-hbase.sh
+++ pinpoint-2.1.0/pinpoint-hbase/build/scripts/initialize-hbase.sh
@@ -7,7 +7,7 @@ if [ -z $DEPEND_SERVICE ]; then
     /opt/hbase/hbase-1.2.6/bin/start-hbase.sh
     /usr/local/bin/check-table.sh
 else    
-    ZOOKEEPER_ADDRESS=$(nslookup ${DEPEND_SERVICE%:*} | grep Name | awk '{print $2}')
+    ZOOKEEPER_ADDRESS=$(nslookup ${DEPEND_SERVICE%:*} | grep Address | sed -n 2p | awk '{print $2}')
     sed -i "s/zk/$ZOOKEEPER_ADDRESS/g" /opt/hbase/hbase-1.2.6/conf/hbase-site.xml
     /opt/hbase/hbase-1.2.6/bin/start-hbase.sh
     /usr/local/bin/check-table.sh
