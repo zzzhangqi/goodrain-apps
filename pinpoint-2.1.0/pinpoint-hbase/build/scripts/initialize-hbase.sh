6d9b72b441183c94c8c2953a5dfff9b6ea3ca964
zzzhangqi <13651281168@163.com>
2021-05-06 18:34:16 +0800

    update zk

--- pinpoint-2.1.0/pinpoint-hbase/build/scripts/initialize-hbase.sh
+++ pinpoint-2.1.0/pinpoint-hbase/build/scripts/initialize-hbase.sh
@@ -7,8 +7,9 @@ if [ -z $DEPEND_SERVICE ]; then
     /opt/hbase/hbase-1.2.6/bin/start-hbase.sh
     /usr/local/bin/check-table.sh
 else    
-    ZOOKEEPER_ADDRESS=$(nslookup ${DEPEND_SERVICE%:*} | grep Address | sed -n 2p | awk '{print $2}')
-    sed -i "s/zk/$ZOOKEEPER_ADDRESS/g" /opt/hbase/hbase-1.2.6/conf/hbase-site.xml
+    # ZOOKEEPER_ADDRESS=$(nslookup ${DEPEND_SERVICE%:*} | grep Address | sed -n 2p | awk '{print $2}')
+    # sed -i "s/zk/$ZOOKEEPER_ADDRESS/g" /opt/hbase/hbase-1.2.6/conf/hbase-site.xml
+    /zookeeper/zookeeper-3.4.0/bin/zkServer.sh start
     /opt/hbase/hbase-1.2.6/bin/start-hbase.sh
     /usr/local/bin/check-table.sh
 fi
