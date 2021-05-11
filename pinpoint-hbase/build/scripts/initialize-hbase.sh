bc45d18337e72adf25fa1855ae289f98c6b2d6b2
zzzhangqi <13651281168@163.com>
2021-05-11 16:38:05 +0800

    update hdfs

--- pinpoint-hbase/build/scripts/initialize-hbase.sh
+++ pinpoint-hbase/build/scripts/initialize-hbase.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
   
+ZOOKEEPER_ADDRESS=$(nslookup ${DEPEND_SERVICE%:*} | grep Address | sed -n 2p | awk '{print $2}')
+sed -i "s/zk/$ZOOKEEPER_ADDRESS/g" /opt/hbase/hbase-1.6.0/conf/hbase-site.xml
 /opt/hbase/hbase-1.6.0/bin/start-hbase.sh
 #/usr/local/bin/check-table.sh
