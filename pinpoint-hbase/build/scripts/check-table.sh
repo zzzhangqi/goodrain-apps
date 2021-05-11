6770bfa79fd36fda279325913c247dc32b0cc1ef
zzzhangqi <13651281168@163.com>
2021-05-11 10:46:16 +0800

    update hbase 1.2.3

--- pinpoint-hbase/build/scripts/check-table.sh
+++ pinpoint-hbase/build/scripts/check-table.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if echo -e "exists 'AgentInfo'" | /opt/hbase/hbase-1.4.0/bin/hbase shell 2>&1 | grep -q "does exist" 2>/dev/null
+if echo -e "exists 'AgentInfo'" | /opt/hbase/hbase-1.2.3/bin/hbase shell 2>&1 | grep -q "does exist" 2>/dev/null
 then 
     echo "Tables already exist"
     break
 else
     sleep 15        
     echo "create tables"
-    /opt/hbase/hbase-1.4.0/bin/hbase shell /opt/hbase/hbase-create.hbase
+    /opt/hbase/hbase-1.2.3/bin/hbase shell /opt/hbase/hbase-create.hbase
 fi
--- pinpoint-hbase/build/scripts/initialize-hbase.sh
+++ pinpoint-hbase/build/scripts/initialize-hbase.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
   
-/opt/hbase/hbase-1.4.0/bin/start-hbase.sh
+/opt/hbase/hbase-1.2.3/bin/start-hbase.sh
 #/usr/local/bin/check-table.sh
