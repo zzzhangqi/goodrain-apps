d2304f42edd6a992cb6ac013825495eae1f27f5f
zzzhangqi <13651281168@163.com>
2021-08-02 18:19:20 +0800

    Add mongodb Sharded Cluster to Rainbond apps

---
+++ mongodb-sharded-cluster/4.4/debian-10/rootfs/opt/bitnami/scripts/mongodb-sharded/rainbond.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+# set -o xtrace # Uncomment this line for debugging purposes
+
+if [[ "$MONGODB_SHARDING_MODE" = "mongos" ]]; then
+    export MONGODB_CFG_PRIMARY_HOST=$(nslookup ${DEPEND_SERVICE%:*} | grep Name | awk '{print $2}' | cut -d . -f -1 | sort --version-sort -r | uniq)
+fi
+
+if [[ "$MONGODB_SHARDING_MODE" = "shardsvr" ]]; then
+    export MONGODB_MONGOS_HOST=$(nslookup ${DEPEND_SERVICE%:*} | grep Name | awk '{print $2}' | cut -d . -f -1 | sort --version-sort -r | uniq)
+fi
