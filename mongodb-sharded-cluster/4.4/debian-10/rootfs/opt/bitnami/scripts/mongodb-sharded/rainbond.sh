66f049e13e2d0d51376a5701c67cf22a08a9c74f
zzzhangqi <13651281168@163.com>
2021-08-03 15:36:24 +0800

    change mongo

--- mongodb-sharded-cluster/4.4/debian-10/rootfs/opt/bitnami/scripts/mongodb-sharded/rainbond.sh
+++ mongodb-sharded-cluster/4.4/debian-10/rootfs/opt/bitnami/scripts/mongodb-sharded/rainbond.sh
@@ -6,15 +6,15 @@ set -o pipefail
 # set -o xtrace # Uncomment this line for debugging purposes
 
 if [[ "$MONGODB_SHARDING_MODE" = "mongos" ]]; then
-    export MONGODB_CFG_PRIMARY_HOST=$(nslookup ${DEPEND_SERVICE%:*} | grep Name | awk '{print $2}' | cut -d . -f -1 | sort --version-sort -r | uniq)
+    export MONGODB_CFG_PRIMARY_HOST=$(nslookup ${DEPEND_SERVICE%:*} | grep Name | awk '{print $2}' | cut -d . -f -1 | sort --version-sort -r | uniq).$SERVICE_NAME
 fi
 
 if [[ "$MONGODB_SHARDING_MODE" = "shardsvr" ]]; then
-    export MONGODB_MONGOS_HOST=$(nslookup ${DEPEND_SERVICE%:*} | grep Name | awk '{print $2}' | cut -d . -f -1 | sort --version-sort -r | uniq)
+    export MONGODB_MONGOS_HOST=$(nslookup ${DEPEND_SERVICE%:*} | grep Name | awk '{print $2}' | cut -d . -f -1 | sort --version-sort -r | uniq).$SERVICE_NAME
 fi
 
 if [[ "$MONGODB_REPLICA_SET_MODE" = "secondary" ]]; then
-    export MONGODB_PRIMARY_HOST=$(nslookup ${DEPEND_SERVICE%:*} | grep Name | awk '{print $2}' | cut -d . -f -1 | sort --version-sort -r | uniq)
+    export MONGODB_PRIMARY_HOST=$(nslookup ${DEPEND_SERVICE%:*} | grep Name | awk '{print $2}' | cut -d . -f -1 | sort --version-sort -r | uniq).$SERVICE_NAME
 fi
 
 export MONGODB_ADVERTISED_HOSTNAME=$HOSTNAME.$SERVICE_NAME
