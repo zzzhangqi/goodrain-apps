9ae83a4016b74afc1469ee58a01e32f2fdde308f
zzzhangqi <13651281168@163.com>
2021-08-03 14:27:18 +0800

    change mongo

--- mongodb-sharded-cluster/4.4/debian-10/rootfs/opt/bitnami/scripts/mongodb-sharded/rainbond.sh
+++ mongodb-sharded-cluster/4.4/debian-10/rootfs/opt/bitnami/scripts/mongodb-sharded/rainbond.sh
@@ -13,4 +13,8 @@ if [[ "$MONGODB_SHARDING_MODE" = "shardsvr" ]]; then
     export MONGODB_MONGOS_HOST=$(nslookup ${DEPEND_SERVICE%:*} | grep Name | awk '{print $2}' | cut -d . -f -1 | sort --version-sort -r | uniq)
 fi
 
-export MONGODB_ADVERTISED_HOSTNAME=$HOSTNAME
+if [[ "$MONGODB_REPLICA_SET_MODE" = "secondary" ]]; then
+    export MONGODB_PRIMARY_HOST=$(nslookup ${DEPEND_SERVICE%:*} | grep Name | awk '{print $2}' | cut -d . -f -1 | sort --version-sort -r | uniq)
+fi
+
+export MONGODB_ADVERTISED_HOSTNAME=$HOSTNAME.$SERVICE_NAME
